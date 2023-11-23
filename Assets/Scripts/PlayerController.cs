c16d6d88fffdef9b2e92fc0f79db72ecfc6af596
sasakiy916 <sasaki3y98521j@gmail.com>
2023-11-23 22:45:25 +0900

    サンプル完成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    NavMeshAgent nav;
+    public GameObject target;
+    public Text debugText;
+    Animator anim;
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        nav.destination = target.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // debugText.text = $"Position:{transform.position}";
+        debugText.text = $"Position:{nav.desiredVelocity.magnitude / nav.speed}";
+        anim.SetFloat("run", nav.desiredVelocity.magnitude / nav.speed);
     }
 }
